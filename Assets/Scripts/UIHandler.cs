4bc7e3e1b33467abe9a8dfa833d82488d4acfc73
Alex Lementuev <a.lementuev@gmail.com>
2017-01-14 03:17:30 -0800

    Added Android runtime

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ApptentiveConnect;
+
+public class UIHandler : MonoBehaviour
+{
+    public void OnEngageButton()
+    {
+        Apptentive.sharedConnection.Engage("test_event");
+    }
+
+    public void OnMessageCenterButton()
+    {
+        Apptentive.sharedConnection.PresentMessageCenter();
+    }
+}
